877ebca38876401ff37b3a43df722816cd97f611
Connor Kuehl <connorkuehl95@gmail.com>
2016-07-30 10:25:07 -0700

    Initial outline of runCycle method. Switch statement decodes all 35 of the opcodes.

--- Chip8.cpp
+++ Chip8.cpp
@@ -31,7 +31,179 @@ void Chip8::loadROM(const std::string& romFile)
 }
 
 void Chip8::runCycle()
-{}
+{
+    // Load the two byte quantity for decoding
+    opcode = memory[pc] << 8 | memory[pc + 1];
+
+    // Isolate highest 4 bits which contain opcode
+    switch (opcode & 0xF000)
+    {
+        /*
+         * Special case: three opcodes start with 0x0 as highest 4 bits, but are distinguishible
+         *               by the last 4 bits (i.e, 0NNN (syscall), 00E0(clear screen), 00EE(ret)
+         *                                           ^               ^                   ^
+         */
+        case 0x0000:
+            switch (opcode & 0x000F)
+            {
+                // 0x0NNN - unused, this is a chip8 system call
+                // 0x00E0 - clears the screen
+                case 0x0000: 
+                    break;
+                // 0x00EE - returns from a function call
+                case 0x000E: 
+                     break;
+                default: // TODO error handle bad opcode
+                     break;
+            }
+            break;
+        // 0x1NNN - JMP to address at address `NNN`
+        case 0x1000:
+            break;
+        // 0x2NNN - CALL subroutine at address `NNN`
+        case 0x2000:
+            break;
+        // 0x3XNN - SKIP next instruction if VX == `NN`
+        case 0x3000:
+            break;
+        // 0x4XNN - SKIP next instruction if VX != `NN`
+        case 0x4000:
+            break;
+        // 0x5XY0 - SKIP next instruction if VX == VY
+        case 0x5000:
+            break;
+        // 0x6XNN - SET VX = `NN`
+        case 0x6000:
+            break;
+        // 0x7XNN - SET VX += `NN`
+        case 0x7000:
+            break;
+        /*
+         * Special case: multiple opcodes start with 0x8 as highest 4 bits
+         */
+        case 0x8000:
+            switch (opcode & 0x000F)
+            {
+                // 0x8XY0 - SET VX = VY
+                case 0x0000:
+                    break;
+                // 0x8XY1 - SET VX = VX | VY
+                case 0x0001:
+                    break;
+                // 0x8XY2 - SET VX = VX & VY
+                case 0x0002:
+                    break;
+                // 0x8XY3 - SET VX = VX ^ VY
+                case 0x0003:
+                    break;
+                // 0x8XY4 - SET VX += VY (VF is set to 1 if there's a carry, 0 if not)
+                case 0x0004:
+                    break;
+                // 0x8XY5 - SET VX -= VY (VF is set to 0 if there's a borrow, 1 if not)
+                case 0x0005:
+                    break;
+                // 0x8XY6 - SET VX = VX >> 1 (VF is LSB of VX prior to shift)
+                case 0x0006:
+                    break;
+                // 0x8XY7 - SET VX = VY - VX (VF is set to 0 if there's a borrow, 1 if not)
+                case 0x0007:
+                    break;
+                // 0x8XYE - SET VX = VX << 1 (VF is MSB of VX prior to shift)
+                case 0x000E:
+                    break;
+                default: //TODO error handle bad opcode
+                    break;
+            }
+            break;
+        // 0x9XY0 - SKIP next instruction if VX != VY
+        case 0x9000:
+            break;
+        // 0xANNN - SET I = `NNN`
+        case 0xA000:
+            break;
+        // 0xBNNN - JMP to address `NNN` + V0
+        case 0xB000:
+            break;
+        // 0xCXNN - SET VX = randomNum & NN
+        case 0xC000:
+            break;
+        // 0xDXYN - Draw sprite at coord (VX, VY) with a width of 8 pixels and height of N pixels
+        //          (VF is set to 1 if any screen pixels are flipped from set to unset when the
+        //          sprite is drawn and 0 if that does not happen.
+        case 0xD000:
+            break;
+        /*
+         * Special case: multiple opcodes start with 0xE as highest 4 bits
+         */
+        case 0xE000:
+            switch (opcode & 0x000F)
+            {
+                // 0xEX9E - SKIP next instruction if key stored in VX is pressed
+                case 0x000E:
+                    break;
+                // 0xEXA1 - SKIP next instruction if key stored in VX ISN'T pressed
+                case 0x0001:
+                    break;
+                default: // TODO error handling for bad opcode
+                    break;
+            }
+            break;
+        /*
+         * Special case: multiple opcodes start with 0xF as highest 4 bits
+         */
+        case 0xF000:
+            switch (opcode & 0x000F)
+            {
+                // 0xFX07 - SET VX = delayTimer
+                case 0x0007:
+                    break;
+                // 0xFX0A - Wait for keypress, then store it in VX
+                case 0x000A:
+                    break;
+                // Sub special case: multiple opcodes ending with 0x
+                case 0x0005:
+                    switch (opcode & 0x00F0)
+                    {
+                        // 0xFX15 - SET delay timer to VX
+                        case 0x0010:
+                            break;
+                        // 0xFX55 - Stores V0 through VX in memory starting at address I
+                        case 0x0050:
+                            break;
+                        // 0xFX65 - Fills V0 through VX with vaues in memory starting at addr I
+                        case 0x0060:
+                            break;
+                        default: // TODO error handling for bad opcode
+                            break;
+                    }
+                    break;
+                // 0xFX18 - SET sound timer to VX
+                case 0x0008:
+                    break;
+                // 0xFX1E - SET I += VX
+                case 0x000E:
+                    break;
+                // 0xFX29 - SET I to location of sprite in VX, 0-F in hex are a 4x5 font
+                case 0x0009:
+                    break;
+                // 0xFX33 - Store decimal parts of VX - I = hundreds, I+1 = tens, I+2 = ones
+                case 0x0003:
+                    break;
+            }
+            break;
+    }
+
+    if (delayTimer > 0)
+        --delayTimer;
+
+    if (soundTimer > 0)
+    {
+        if (soundTimer == 1)
+            // TODO PLAY SOUND
+        --soundTimer;
+    }
+
+}
 
 void Chip8::draw()
 {}
